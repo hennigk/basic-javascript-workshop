f721584f1356d546ad346f0550dd35c53a24afbf
hennigk <kayla.hennig@gmail.com>
2015-09-11 18:11:52 +0000

    added lastChar function

--- answers.js
+++ answers.js
@@ -1,12 +1,23 @@
 //a function that takes returns the first character of a string
 function firstChar(aString){
-    if (aString != undefined) {
+    if (aString != "") {
     return aString.charAt(0);
 }
     else {
-        return "null";
+        return "empty string!";
     }
 };
 console.log(firstChar("Kayla"));
 console.log(firstChar(""));
-console.log(firstChar());
+
+//a function that takes returns the last character of a string
+function lastChar(lastString){
+    if (lastString != "") {
+    return lastString.charAt((lastString).length - 1);
+}
+    else {
+        return "empty string!";
+    }
+};
+console.log(lastChar("Kayla"));
+console.log(lastChar(""));
