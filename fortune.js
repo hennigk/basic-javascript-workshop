79279468ce1c057a6b386e250d054c188531a22f
hennigk <kayla.hennig@gmail.com>
2015-09-13 17:49:45 +0000

    added fortune file

---
+++ fortune.js
@@ -0,0 +1,22 @@
+When one burns one's bridges, what a very nice fire it makes.
+		-- Dylan Thomas
+Your love life will be happy and harmonious.
+Go not to the elves for counsel, for they will say both yes and no.
+		-- J.R.R. Tolkien
+You are taking yourself far too seriously.
+Don't get stuck in a closet -- wear yourself out.
+You will pioneer the first Martian colony.
+You will be married within a year.
+Exercise caution in your daily affairs.
+You are scrupulously honest, frank, and straightforward.  Therefore you
+have few friends.
+You will be attacked by a beast who has the body of a wolf, the tail of
+a lion, and the face of Donald Duck.
+If one cannot enjoy reading a book over and over again, there is no use
+in reading it at all.
+		-- Oscar Wilde
+"... an experienced, industrious, ambitious, and often quite often
+picturesque liar."
+		-- Mark Twain
+The abuse of greatness is when it disjoins remorse from power.
+		-- William Shakespeare, "Julius Caesar"
